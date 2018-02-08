2ffb87e9fe140a2f7ca671802f21d70ff31b98ff
Wynston <wramsay@hotmail.ca>
2018-02-07 18:02:19 -0600

    input_6.c is complete and working with c.Grm
    Testing file for c.Grm tests various scenarios emphasizing nests.

---
+++ assignment 1/MiniCgrammar/input_6.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<math.h>
+#include<time.h>
+
+/* CMPT 470, Assignment 1
+   Wynston Ramsay
+   Darvin Zhang
+*/
+int main()
+{
+   int i,j,k,x,y,z;
+   char input;
+
+   printf("Which type of iteration method is your favourite to implement:");
+   printf("1) For-Loop");
+   printf("2) While-Loop");
+   printf("3) Do-While-Loop");
+   printf("4) Recursion");
+   scanf("%c", input);
+
+   switch (input)
+   {
+      case '1':
+         printf("For-Loop was chosen\n");
+         for(i=0;i<100;i++){
+            x = i * 3;
+            printf("x:%d", x);
+         }
+         break;
+      case '2':
+         printf("While-Loop was chosen\n");
+            j = 0;
+            while(j<200){
+               y = j + 42;
+               printf("y:%d", y);
+               j++;
+            }
+         break;
+      case '3':
+         printf("Do-While-Loop was chosen\n");
+         k=0;
+         do{
+            z = x + y;
+            printf("z:%d",z);
+            k++;
+         }while(k<300);
+         break;
+      case '4':
+         printf("Recursion was chosen\n");
+         printf("Which type of iteration method is your favourite to implement:");
+         printf("1) For-Loop");
+         printf("2) While-Loop");
+         printf("3) Do-While-Loop");
+         printf("4) Recursion");
+         scanf("%c", input);
+
+         switch (input)
+         {
+            case '1':
+               printf("For-Loop was chosen\n");
+               for(i=0;i<100;i++){
+                  x = i * 3;
+                  printf("x:%d", x);
+               }
+               break;
+            case '2':
+               printf("While-Loop was chosen\n");
+                  j = 0;
+                  while(j<200){
+                     y = j + 42;
+                     printf("y:%d", y);
+                     j++;
+                  }
+               break;
+            case '3':
+               printf("Do-While-Loop was chosen\n");
+               k=0;
+               do{
+                  z = x + y;
+                  printf("z:%d",z);
+                  k++;
+               }while(k<300);
+               break;
+            case '4':
+               printf("Recursion was chosen\n");
+               break;
+            default:
+               printf("You entered an invalid choice\n");
+         }
+         break;
+      default:
+         printf("You entered an invalid choice\n");
+   }
+   return 0;
+}
