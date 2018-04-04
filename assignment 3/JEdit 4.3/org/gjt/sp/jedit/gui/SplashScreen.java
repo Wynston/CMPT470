bbe1dbe7409064d17faba1c977ef68728a1fc358
Darvin Zhang <darvin.zhang@usask.ca>
2018-04-03 23:16:49 -0600

    Finished “Modify the splash window” to scroll our group names on initialization

--- assignment 3/JEdit 4.3/org/gjt/sp/jedit/gui/SplashScreen.java
+++ assignment 3/JEdit 4.3/org/gjt/sp/jedit/gui/SplashScreen.java
@@ -21,6 +21,7 @@ package org.gjt.sp.jedit.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 import org.gjt.sp.jedit.jEdit;
 import org.gjt.sp.util.Log;
@@ -29,29 +30,37 @@ import org.gjt.sp.util.Log;
  * The splash screen displayed on startup.
  * @version $Id: SplashScreen.java 8122 2006-11-24 11:29:49Z kpouer $
  */
-public class SplashScreen extends JComponent
-{
-	public SplashScreen()
-	{
+public class SplashScreen extends JComponent {
+	public SplashScreen() {
 		setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 		setBackground(Color.white);
 
-		Font font = new Font("Dialog",Font.PLAIN,10);
+		Font font = new Font("Dialog", Font.PLAIN, 10);
 		setFont(font);
 		fm = getFontMetrics(font);
 
-		image = getToolkit().getImage(
-			getClass().getResource("/org/gjt/sp/jedit/icons/splash.png"));
+		image = getToolkit().getImage(getClass().getResource("/org/gjt/sp/jedit/icons/splash.png"));
 		MediaTracker tracker = new MediaTracker(this);
-		tracker.addImage(image,0);
+		tracker.addImage(image, 0);
+		
+		// #1 - create a vector to hold our group member names
+		members = new Vector(50);
+		members.addElement("Wynston Ramsay - wcr723");
+		members.addElement("Evan Snook - egs819");
+		members.addElement("Darvin Zhang - ddz369");
+		
+		// #1 - initialize the starting position of the scrolling text
+		scrPos = -300;
+		
+		// #1 - maxWidth, given the longest identification
+		maxWidth = fm.stringWidth("Wynston Ramsay - wcr723") + 10;
 
-		try
-		{
+		animThread = new AnimationThread();
+		
+		try {
 			tracker.waitForAll();
-		}
-		catch(Exception e)
-		{
-			Log.log(Log.ERROR,this,e);
+		} catch (Exception e) {
+			Log.log(Log.ERROR, this, e);
 		}
 
 		win = new JWindow();
@@ -60,43 +69,35 @@ public class SplashScreen extends JComponent
 		int height = gs[0].getDisplayMode().getHeight();
 		int width = gs[0].getDisplayMode().getWidth();
 		Dimension screen = new Dimension(width, height);
-		Dimension size = new Dimension(image.getWidth(this) + 2,
-			image.getHeight(this) + 2 + PROGRESS_HEIGHT);
+		Dimension size = new Dimension(image.getWidth(this) + 2, image.getHeight(this) + 2 + PROGRESS_HEIGHT);
 		win.setSize(size);
 
 		win.getContentPane().add(this, BorderLayout.CENTER);
 
-		win.setLocation((screen.width - size.width) / 2,
-			(screen.height - size.height) / 2);
+		win.setLocation((screen.width - size.width) / 2, (screen.height - size.height) / 2);
 		win.validate();
 		win.setVisible(true);
 	}
 
-	public void dispose()
-	{
+	public void dispose() {
 		win.dispose();
 	}
 
-	public synchronized void advance()
-	{
+	public synchronized void advance() {
 		logAdvanceTime(null);
 		progress++;
 		repaint();
 
 		// wait for it to be painted to ensure progress is updated
 		// continuously
-		try
-		{
+		try {
 			wait();
-		}
-		catch(InterruptedException ie)
-		{
-			Log.log(Log.ERROR,this,ie);
+		} catch (InterruptedException ie) {
+			Log.log(Log.ERROR, this, ie);
 		}
 	}
 
-	public synchronized void advance(String label)
-	{
+	public synchronized void advance(String label) {
 		logAdvanceTime(label);
 		progress++;
 		this.label = label;
@@ -104,25 +105,19 @@ public class SplashScreen extends JComponent
 
 		// wait for it to be painted to ensure progress is updated
 		// continuously
-		try
-		{
+		try {
 			wait();
-		}
-		catch(InterruptedException ie)
-		{
-			Log.log(Log.ERROR,this,ie);
+		} catch (InterruptedException ie) {
+			Log.log(Log.ERROR, this, ie);
 		}
 	}
 
-	private void logAdvanceTime(String label)
-	{
+	private void logAdvanceTime(String label) {
 		long currentTime = System.currentTimeMillis();
-		if (lastLabel != null)
-		{
-			Log.log(Log.DEBUG, SplashScreen.class, lastLabel +':'+(currentTime - lastAdvanceTime) + "ms");
+		if (lastLabel != null) {
+			Log.log(Log.DEBUG, SplashScreen.class, lastLabel + ':' + (currentTime - lastAdvanceTime) + "ms");
 		}
-		if (label != null)
-		{
+		if (label != null) {
 			lastLabel = label;
 			lastAdvanceTime = currentTime;
 
@@ -130,36 +125,82 @@ public class SplashScreen extends JComponent
 
 	}
 
-	public synchronized void paintComponent(Graphics g)
-	{
+	public synchronized void paintComponent(Graphics g) {
 		Dimension size = getSize();
 
 		g.setColor(Color.black);
-		g.drawRect(0,0,size.width - 1,size.height - 1);
+		g.drawRect(0, 0, size.width - 1, size.height - 1);
 
-		g.drawImage(image,1,1,this);
+		g.drawImage(image, 1, 1, this);
 
 		// XXX: This should not be hardcoded
 		g.setColor(Color.white);
-		g.fillRect(1,image.getHeight(this) + 1,
-			((win.getWidth() - 2) * progress) / PROGRESS_COUNT, PROGRESS_HEIGHT);
+		g.fillRect(1, image.getHeight(this) + 1, ((win.getWidth() - 2) * progress) / PROGRESS_COUNT, PROGRESS_HEIGHT);
 
 		g.setColor(Color.black);
 
-		if (label != null)
-		{
-			g.drawString(label,
-				     (getWidth() - fm.stringWidth(label)) / 2,
-				     image.getHeight(this) + (PROGRESS_HEIGHT
-							      + fm.getAscent() + fm.getDescent()) / 2);
+		if (label != null) {
+			g.drawString(label, (getWidth() - fm.stringWidth(label)) / 2,
+					image.getHeight(this) + (PROGRESS_HEIGHT + fm.getAscent() + fm.getDescent()) / 2);
 		}
 
-
 		String version = jEdit.getVersion();
-		g.drawString(version,
-			getWidth() - fm.stringWidth(version) - 2,
-			image.getHeight(this) - fm.getDescent());
+		g.drawString(version, getWidth() - fm.stringWidth(version) - 2, image.getHeight(this) - fm.getDescent());
+
+		// #1 - Draw the our members out, per line, given a starting position
+		int height = fm.getHeight();
+		int firstLine = scrPos / height;
+		int lines = getHeight() / height;
+		int y = fm.getHeight();
+		for (int i = 0; i <= lines; i++) {
+			if (i + firstLine >= 0 && i + firstLine < members.size()) {
+				String line = (String)members.get(i + firstLine);
+				g.drawString(line, 20, y);
+			}
+			y += fm.getHeight();
+		}
+
 		notify();
+	}
+
+	public void addNotify() {
+		super.addNotify();
+		animThread.start();
+	}
+
+	public void removeNotify() {
+		super.removeNotify();
+		animThread.kill();
+	}
+
+	// #1 - AnimationThread. Continually move the screen
+	// position of our lines upwards, and redraw them
+	// to create a scrolling effect
+	public class AnimationThread extends Thread {
+		private boolean isRunning;
+
+		AnimationThread() {
+			isRunning = true;
+			setPriority(Thread.MIN_PRIORITY);
+		}
+
+		public void kill() {
+			isRunning = false;
+		}
+
+		public void run() {
+			while (isRunning) {
+				scrPos += 5;
+				
+				try {
+					animThread.sleep(10);
+				} catch (Exception ex) {
+					System.out.println(ex.toString());
+				}
+
+				repaint(getWidth() / 2 - maxWidth, 0, maxWidth * 2, getHeight());
+			}
+		}
 	}
 
 	// private members
@@ -172,4 +213,9 @@ public class SplashScreen extends JComponent
 	private String label;
 	private String lastLabel;
 	private long lastAdvanceTime = System.currentTimeMillis();
+	// #1 - variables
+	private Vector<String> members;
+	private int maxWidth;
+	private int scrPos;
+	private AnimationThread animThread;
 }
